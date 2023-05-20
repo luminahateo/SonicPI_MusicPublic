b946eb8cafd56aa5708cb4629f16420009903e1a
Hoffmann Julien <29329710+luminahateo@users.noreply.github.com>
2023-05-20 23:15:58 +0200

    Exo_Prelude_C_Bach

---
+++ prel_C_Bach.rb
@@ -0,0 +1,378 @@
+# J.S. Bach - Prelude in C Major
+# Adaptation HOFFMANN Julien aka Luminahateo
+# Exercice 30min 
+
+
+
+#Variables
+notetmp=0.2
+litude=0.5
+pedal=1
+use_synth :piano
+
+
+#Program
+2.times do
+  play 60, decay: pedal
+  sleep notetmp
+  play 64, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 67, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal
+    sleep notetmp
+    play 76, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 60, decay: pedal
+  sleep notetmp
+  play 62, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 69, decay: pedal
+    sleep notetmp
+    play 74, decay: pedal
+    sleep notetmp
+    play 77, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 59, decay: pedal
+  sleep notetmp
+  play 62, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 67, decay: pedal
+    sleep notetmp
+    play 74, decay: pedal
+    sleep notetmp
+    play 77, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 60, decay: pedal
+  sleep notetmp
+  play 64, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 67, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal
+    sleep notetmp
+    play 76, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 57, decay: pedal
+  sleep notetmp
+  play 67, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 72, decay: pedal
+    sleep notetmp
+    play 76, decay: pedal
+    sleep notetmp
+    play 81, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 62, decay: pedal
+  sleep notetmp
+  play 62, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 66, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal
+    sleep notetmp
+    play 74, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 59, decay: pedal
+  sleep notetmp
+  play 62, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 67, decay: pedal
+    sleep notetmp
+    play 74, decay: pedal
+    sleep notetmp
+    play 79, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 60, decay: pedal
+  sleep notetmp
+  play 62, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 64, decay: pedal
+    sleep notetmp
+    play 67, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 57, decay: pedal
+  sleep notetmp
+  play 62, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 64, decay: pedal
+    sleep notetmp
+    play 67, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 52, decay: pedal
+  sleep notetmp
+  play 62, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 62, decay: pedal
+    sleep notetmp
+    play 66, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 55, decay: pedal
+  sleep notetmp
+  play 59, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 62, decay: pedal
+    sleep notetmp
+    play 67, decay: pedal
+    sleep notetmp
+    play 71, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 55, decay: pedal
+  sleep notetmp
+  play 70, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 64, decay: pedal
+    sleep notetmp
+    play 70, decay: pedal
+    sleep notetmp
+    play 73, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 53, decay: pedal
+  sleep notetmp
+  play 57, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 62, decay: pedal
+    sleep notetmp
+    play 69, decay: pedal
+    sleep notetmp
+    play 74, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 53, decay: pedal
+  sleep notetmp
+  play 57, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 62, decay: pedal
+    sleep notetmp
+    play 68, decay: pedal
+    sleep notetmp
+    play 71, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 60, decay: pedal
+  sleep notetmp
+  play 62, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 64, decay: pedal
+    sleep notetmp
+    play 67, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 53, decay: pedal
+  sleep notetmp
+  play 60, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 69, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal
+    sleep notetmp
+    play 77, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 62, decay: pedal
+  sleep notetmp
+  play 67, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 69, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal
+    sleep notetmp
+    play 77, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 55, decay: pedal
+  sleep notetmp
+  play 59, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 67, decay: pedal
+    sleep notetmp
+    play 71, decay: pedal
+    sleep notetmp
+    play 77, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+2.times do
+  play 60, decay: pedal
+  sleep notetmp
+  play 64, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 67, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal
+    sleep notetmp
+    play 76, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 60, decay: pedal
+  sleep notetmp
+  play 64, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 70, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal
+    sleep notetmp
+    play 76, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 57, decay: pedal
+  sleep notetmp
+  play 65, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 69, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal
+    sleep notetmp
+    play 76, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 58, decay: pedal
+  sleep notetmp
+  play 60, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 66, decay: pedal
+    sleep notetmp
+    play 69, decay: pedal
+    sleep notetmp
+    play 75, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 58, decay: pedal
+  sleep notetmp
+  play 63, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 65, decay: pedal
+    sleep notetmp
+    play 70, decay: pedal
+    sleep notetmp
+    play 73, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+2.times do
+  play 58, decay: pedal
+  sleep notetmp
+  play 63, decay: pedal, amp: litude
+  sleep notetmp
+  2.times do
+    play 65, decay: pedal
+    sleep notetmp
+    play 69, decay: pedal
+    sleep notetmp
+    play 72, decay: pedal, amp: litude
+    sleep notetmp
+  end
+end
+
+play 60, decay: pedal, amp: litude
+sleep notetmp+0.2
+play 64, decay: pedal, amp: 0.4
+sleep notetmp+0.1
+play 72, decay: pedal, amp: 0.3
