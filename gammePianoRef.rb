457c8931868524691c461474597b778cb202f6e4
Hoffmann Julien <29329710+luminahateo@users.noreply.github.com>
2023-05-27 19:08:32 +0200

    Create gammePianoRef.rb

---
+++ gammePianoRef.rb
@@ -0,0 +1,52 @@
+# Gamme piano ref
+
+#Variables
+notetmp=0.2
+litude=1
+pedal=0.5
+use_synth :piano
+
+play :C3, decay: pedal, amp: litude
+sleep 0.3
+play :D3, decay: pedal, amp: litude
+sleep 0.3
+play :E3, decay: pedal, amp: litude
+sleep 0.3
+play :F3, decay: pedal, amp: litude
+sleep 0.3
+play :G3, decay: pedal, amp: litude
+sleep 0.3
+play :A3, decay: pedal, amp: litude
+sleep 0.3
+play :B3, decay: pedal, amp: litude
+sleep 0.3
+play :C4, decay: pedal, amp: litude
+sleep 0.3
+
+
+play :C3, decay: pedal, amp: litude
+sleep 0.3
+play :Cs3, decay: pedal, amp: litude # "s" pour le Dieze et "b" pour le bemol
+sleep 0.3
+play :D3, decay: pedal, amp: litude
+sleep 0.3
+play :Ds3, decay: pedal, amp: litude
+sleep 0.3
+play :E3, decay: pedal, amp: litude
+sleep 0.3
+play :F3, decay: pedal, amp: litude
+sleep 0.3
+play :Fs3, decay: pedal, amp: litude
+sleep 0.3
+play :G3, decay: pedal, amp: litude
+sleep 0.3
+play :Gs3, decay: pedal, amp: litude
+sleep 0.3
+play :A3, decay: pedal, amp: litude
+sleep 0.3
+play :As3, decay: pedal, amp: litude
+sleep 0.3
+play :B3, decay: pedal, amp: litude
+sleep 0.3
+play :C4, decay: pedal, amp: litude
+sleep 0.3
